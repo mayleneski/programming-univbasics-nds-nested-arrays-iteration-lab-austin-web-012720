2ca66bdbefee12ee8d9e0554035d95813c13f7c8
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-03 05:04:47 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,17 +1,5 @@
 def join_ingredients(src)
-  output = []
-  row_index = 0
-  while row_index <  src.count do 
-    element_index = 0
-    
-    while element_index < src[row_index].count do
-      output << "I love #{src[row_index][element_index]} and #{src[row_index + 1][element_index + 1]} on my pizza"
-      element_index += 1
-    end
-  row_index += 1
-end
-
-output
+  
 
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
